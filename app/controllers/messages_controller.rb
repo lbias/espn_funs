29b66ef5608d98e45520783e939a7cd2853fe7cf
sam <morvazhu@gmail.com>
2017-04-18 10:24:49 +0800

    new and create action

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,25 @@
 class MessagesController < ApplicationController
+  
   def index
-  end 
+  end
+
+  def new
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new
+    if @message.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:title,:description)
+  end
+
 end
